9669b845f55e68e0344ff07441df37d9fbaecd7d
younus-always <younusofficial443@gmail.com>
2025-09-21 19:01:25 +0600

    project initilized

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="bg-gray-200 min-h-screen overflow-hidden">
+      <h1 className="text-4xl font-semibold text-center">Task Planner App</h1>
+    </div>
+  )
+}
